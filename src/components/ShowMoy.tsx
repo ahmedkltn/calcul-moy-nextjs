5be1fdc7d63e8309c24201f6763f83c159cd715b
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-03-21 18:48:14 +0100

    Change Button

--- src/components/ShowMoy.tsx
+++ src/components/ShowMoy.tsx
@@ -1,27 +1,27 @@
-  import React, { useState } from "react";
-  import { Button, Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
-  interface Props {
-      moyenne : Number;
-      isCalculClicked : boolean;
-      handleClose : ()=>void;
-  }
-  const ShowMoy = ({ moyenne, isCalculClicked, handleClose }: Props) => {
-    return (
-      <>
-        <Modal show={isCalculClicked} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title> Moyenne : </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>La moyenne est : {moyenne.toFixed(3)}</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Fermer
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  };
+interface Props {
+  moyenne: Number;
+  isCalculClicked: boolean;
+  handleClose: () => void;
+}
+const ShowMoy = ({ moyenne, isCalculClicked, handleClose }: Props) => {
+  return (
+    <>
+      <Modal show={isCalculClicked} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title> Moyenne : </Modal.Title>
+        </Modal.Header>
+        <Modal.Body >La moyenne est : {moyenne.toFixed(3)}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fermer
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
-  export default ShowMoy;
+export default ShowMoy;
